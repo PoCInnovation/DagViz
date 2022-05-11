09f8e83bac1645623e200144d572f6d65cc75556
Florian Lauch <florian.lauch@epitech.eu>
2022-05-11 13:10:43 +0200

    [ADD] Added functions to build and validate CUE

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"cuelang.org/go/cue/cuecontext"
+	"cuetojson/cuetojson"
 	"fmt"
 	"os"
 )
 
 func main() {
 	args := os.Args
+	context := cuecontext.New()
 
 	if len(args) != 2 {
 		fmt.Println("Error: A CUE file is required")
 		os.Exit(1)
 	}
+
+	value, err := cuetojson.LoadFile(context, args[1])
+
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("%v\n", value)
 }
