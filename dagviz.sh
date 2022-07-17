0a402d06cc067fe82ab11da23b78601ae859c5c0
isma <ismaelfall1901@icloud.com>
2022-07-17 08:03:24 +0200

    update: scripts + clean: repo

--- dagviz.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PATHTOREPO="/home/isma/Developement/Go/DagViz/electron"
-ARG=$1
-if [ -z "$ARG" ]; then
-  ARG=$PWD
-fi
-cd $PATHTOREPO || exit; CUEDIR="-cueDir=\"$ARG\"" npm run dev; cd - || exit;
--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@
 sudo rm -f "/usr/local/bin/dagviz"
 
 cd electron || exit; npm install; cd - || exit;
+cd go || exit; go build -o dagviz; cd - || exit;
 
 echo "#!/bin/bash" > dagviz.sh
 echo "PATHTOREPO=\"$PWD/electron\"" >> dagviz.sh
 echo "ARG=\$1" >> dagviz.sh
 echo "if [ -z \"\$ARG\" ]; then" >> dagviz.sh
 echo "  ARG=\$PWD" >> dagviz.sh
+echo "elif [[ \"\$ARG::1\" != \"/\" ]]; then" >> dagviz.sh
+echo "  NEWARG=\"\$PWD/\$ARG\"" >> dagviz.sh
+echo "  ARG=\$NEWARG" >> dagviz.sh
 echo "fi" >> dagviz.sh
 echo "cd \$PATHTOREPO || exit; CUEDIR=\"-cueDir=\\\"\$ARG\\\"\" npm run dev; cd - || exit;" >> dagviz.sh
 
