617469234626480450a864bfef2c45ee2aff9b79
isma <ismaelfall1901@icloud.com>
2022-07-17 06:54:44 +0200

    update: installations scripts

--- dagviz.sh
+++ dagviz.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 PATHTOREPO="/home/isma/Developement/Go/DagViz/electron"
 ARG=$1
-
 if [ -z "$ARG" ]; then
   ARG=$PWD
 fi
-
-# check the if the directory exists
-
 cd $PATHTOREPO || exit; CUEDIR="-cueDir=\"$ARG\"" npm run dev; cd - || exit;
--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ sudo rm -f "/usr/local/bin/dagviz"
 
 echo "#!/bin/bash" > dagviz.sh
 echo "PATHTOREPO=\"$PWD/electron\"" >> dagviz.sh
-echo "cd \$PATHTOREPO || echo \"can't find the location\"; npm run dev; cd -" >> dagviz.sh
+echo "ARG=\$1" >> dagviz.sh
+echo "if [ -z \"\$ARG\" ]; then" >> dagviz.sh
+echo "  ARG=\$PWD" >> dagviz.sh
+echo "fi" >> dagviz.sh
+echo "cd \$PATHTOREPO || exit; CUEDIR=\"-cueDir=\\\"\$ARG\\\"\" npm run dev; cd - || exit;" >> dagviz.sh
 
 sudo ln -s "$PWD/dagviz.sh" "/usr/local/bin/dagviz"
