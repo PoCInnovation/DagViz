07f5a4a709dfef69d93848ece81323ece2186620
Florian Lauch <florian.lauch@epitech.eu>
2022-07-27 18:34:06 +0200

    add: Mermaid dep

--- viz/src/App.tsx
+++ viz/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import content from './data/data.json';
+import DagVizualizer from "./components/DagVizualizer";
+
+export type DagDefinition = {
+  name: string,
+  file: string,
+  def: string,
+  dependencies: string[],
+}
 
 function App() {
   return (
     <div>
-      {JSON.stringify(content)}
+      {/*JSON.stringify(content)*/}
+      <DagVizualizer data={content}>
+      </DagVizualizer>
     </div>
   );
 }
---
+++ viz/src/components/DagVizualizer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {DagDefinition} from "../App";
+
+export interface DagVizualizerProps {
+    data: DagDefinition[];
+}
+
+export default function DagVizualizer(data: any): JSX.Element {
+    return <div></div>
+}
