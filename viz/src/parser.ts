56df86c071ea888172423434042cf8533271fc79
Florian Lauch <florian.lauch@epitech.eu>
2022-08-04 21:59:29 +0200

    fix: Syntax issues

--- viz/src/parser.ts
+++ viz/src/parser.ts
@@ -2,7 +2,7 @@ import {DagDefinition, DagResults} from './types';
 
 export default function generateChart(dag: DagResults): string {
     const style = "graph LR;"
-    const fileNode = "PARENT[" + dag.file + "]"
+    const fileNode = "PARENT[\"" + dag.file + "\"]"
     let count = 0;
 
     const nodes = dag.dag.map(node => {
@@ -10,12 +10,13 @@ export default function generateChart(dag: DagResults): string {
         return generateNode(node, fileNode, count);
     });
 
+    console.log(style + nodes.join(""))
     return style + nodes.join("");
 }
 
 function generateNode(node: DagDefinition, parent: string, count: number): string {
     const link = parent + "-->"
-    const style = count + "[" + node.name + "];"
+    const style = count + "[\"" + node.name + "\"];"
 
     return link + style
 }
