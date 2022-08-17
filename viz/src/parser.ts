ea89706087e285afc5a00bf90f52d275e41fefbe
Florian Lauch <florian.lauch@epitech.eu>
2022-08-17 21:43:02 +0200

    fix: Removed triple nested function

--- viz/src/parser.ts
+++ viz/src/parser.ts
@@ -1,43 +1,35 @@
-import {DagDefinition, DagResults} from './types';
-import {tab} from "@testing-library/user-event/dist/tab";
-
-
-type generateNodeReturn = {
-    toAdd: string,
-    count: number
-    newNodes: DagDefinition[]
-}
+import { DagDefinition, DagResults } from './types';
 
 export default function generateChart(dag: DagResults): string {
-    const style = "graph LR;"
+    const stack = "graph LR;"
     const fileNode = "PARENT[\"" + dag.file + "\"]"
-    let stack = "graph LR;"
+    const table = generateTable(dag, fileNode)
 
-    function rec() : string[]{
-        let table : string[] = []
-        let count: number = 0;
-        function r(baseNode: DagDefinition, parentNode: string) {
-            const node = generateNode(baseNode, parentNode, count)
-            let nombre: number = count
-            table.push(node)
-            baseNode.dependencies.map(n => {
-                count += 1
-                r(n,  nombre+ "[\"" + baseNode.name + "\"]")
-            })
-        }
+    return stack + table.join("")
+}
 
-        dag.dag.map(n => {
-            console.log("hello\n")
-            count += 1
-            r(n, fileNode);
-        })
-        return table
-    }
+function generateTable(dag: DagResults, fileNode: string): string[] {
+    const table: string[] = []
+    let count: number = 0;
 
-    const table = rec()
+    dag.dag.forEach(n => {
+        count += 1
+        count = appendNodes(n, fileNode, table, count)
+    })
 
-    console.log(table)
-    return stack+ table.join("")
+    return table
+}
+
+function appendNodes(baseNode: DagDefinition, parentNode: string, table: string[], count: number): number {
+    const node = generateNode(baseNode, parentNode, count)
+    const parent: number = count
+
+    table.push(node)
+    baseNode.dependencies.forEach(n => {
+        count += 1
+        count = appendNodes(n,  parent + "[\"" + baseNode.name + "\"]", table, count)
+    })
+    return count
 }
 
 function generateNode(node: DagDefinition, parent: string, count: number): string {
